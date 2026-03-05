d42515cca29af542064693445fd6051c73198678
Stuart Northcott <imorital@hotmail.com>
2026-03-05 14:56:25 +0000

    Add QR code support for MFA setup using QRCoder
    Backend generates TOTP QR code for authenticator apps and passes it to the view as a Base64 string. Frontend displays the QR code image for easy scanning. Added QRCoder NuGet package to project dependencies.

--- WebApp/Pages/Account/AuthenticatorWithMFASetup.cshtml.cs
+++ WebApp/Pages/Account/AuthenticatorWithMFASetup.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using QRCoder;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using WebApp.Data.Accounts;
@@ -37,8 +38,28 @@ public class AuthenticatorWithMFASetupModel : PageModel
 
         await _userManager.ResetAuthenticatorKeyAsync(user);
         var key = await _userManager.GetAuthenticatorKeyAsync(user);
+
+        GenerateQrCode(user.Email, key);
         
         SetupMFA.Key = key;
+    }
+
+    private void GenerateQrCode(string? userEmail, string? secretKey)
+    {
+        var appName = "Udemy Security Web App";
+
+        // 1. Format the URI
+        // Format: otpauth://totp/{Issuer}:{User}?secret={Key}&issuer={Issuer}
+        string uri = $"otpauth://totp/{Uri.EscapeDataString(appName)}:{Uri.EscapeDataString(userEmail)}?secret={secretKey}&issuer={Uri.EscapeDataString(appName)}";
+
+        // 2. Generate the QR Code
+        using var qrGenerator = new QRCodeGenerator();
+        using var qrCodeData = qrGenerator.CreateQrCode(uri, QRCodeGenerator.ECCLevel.Q);
+        using var qrCode = new PngByteQRCode(qrCodeData);
+
+        // 3. Convert to Base64 to pass to the view
+        byte[] qrCodeImage = qrCode.GetGraphic(20);
+        SetupMFA.QrCodeBase64 = Convert.ToBase64String(qrCodeImage);
     }
 
     public async Task<IActionResult> OnPostAsync()
@@ -79,4 +100,6 @@ public class SetupMFAViewModel
     [Required]
     [DisplayName("Security Code")]
     public string SecurityCode { get; set; } = string.Empty;
+
+    public string? QrCodeBase64 { get; internal set; }
 }
